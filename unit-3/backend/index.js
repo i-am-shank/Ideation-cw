8e50e414438118df5639be5d507abe8ead8e1762
i-am-shank <shashanksinha4444@gmail.com>
2024-08-27 21:09:17 +0530

    configuration setup for vercel

--- unit-3/backend/index.js
+++ unit-3/backend/index.js
@@ -7,10 +7,14 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log("Server listening on PORT :", PORT);
+    // console.log("Server listening on PORT :", PORT);
 });
 
 // routes -----------
 const domainRoutes = require("./routes/domainRoutes");
 
 app.use("/api/v1", domainRoutes);
+
+app.get("/", (req, res) => {
+    res.status(200).send("Hello from the API route!");
+});
