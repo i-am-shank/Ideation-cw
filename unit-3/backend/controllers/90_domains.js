1a967b4df9fc7e4a630acf4f3fc82c276808222e
i-am-shank <shashanksinha4444@gmail.com>
2024-08-27 20:07:35 +0530

    90s era details added in API response, but incomplete API

---
+++ unit-3/backend/controllers/90_domains.js
@@ -0,0 +1,126 @@
+exports.getAll90Domains = async (req, res) => {
+    try {
+        const allCourses = [
+            {
+                name: "World Wide Web",
+                year: 1991,
+                domain: "https://web.archive.org/web/20130430143157/http://info.cern.ch/hypertext/WWW/TheProject.html",
+            },
+            {
+                name: "Yahoo",
+                year: 1994,
+                domain: "https://web.archive.org/web/20050307162243/https://kottke.org/plus/misc/yahoo-1994/",
+            },
+            {
+                name: "Microsoft",
+                year: 1994,
+                domain: "https://web.archive.org/web/20140811022456/http://www.microsoft.com/en-us/discover/1994/",
+            },
+            {
+                name: "Kodak",
+                year: 1996,
+                domain: "https://web.archive.org/web/19961108211744/http://www2.kodak.com:80/",
+            },
+            {
+                name: "Atlanta Olympic Games",
+                year: 1996,
+                domain: "https://web.archive.org/web/19961223003700/http://www.atlanta.olympic.org:80/",
+            },
+            {
+                name: "Oracle",
+                year: 1996,
+                domain: "https://web.archive.org/web/19961219131225/http://www.oracle.com:80/",
+            },
+            {
+                name: "Disney Store",
+                year: 1996,
+                domain: "https://web.archive.org/web/19961218212740/http://store.disney.com/",
+            },
+            {
+                name: "McDonald's",
+                year: 1996,
+                domain: "https://web.archive.org/web/19961110083459/http://www.mcdonalds.com/",
+            },
+            {
+                name: "MIT",
+                year: 1996,
+                domain: "https://web.archive.org/web/19961224024947/http://web.mit.edu:80/",
+            },
+            {
+                name: "NASA",
+                year: 1997,
+                domain: "https://web.archive.org/web/19970605230559/http://www.nasa.gov:80/",
+            },
+            {
+                name: "Hotmail",
+                year: 1997,
+                domain: "http://web.archive.org/web/19971210171246/http://hotmail.com:80/",
+            },
+            {
+                name: "Electonic Arts",
+                year: 1997,
+                domain: "https://web.archive.org/web/19970221233250/http://www.ea.com/",
+            },
+            {
+                name: "Adobe",
+                year: 1997,
+                domain: "https://web.archive.org/web/19970625023120/http://www.adobe.com/",
+            },
+            {
+                name: "McAfee",
+                year: 1997,
+                domain: "https://web.archive.org/web/19970707093449/http://www.mcafee.com:80/",
+            },
+            {
+                name: "Google",
+                year: 1998,
+                domain: "https://web.archive.org/web/19981202230410/http://www.google.com/",
+            },
+            {
+                name: "Apple",
+                year: 1998,
+                domain: "https://web.archive.org/web/19980509035420/http://www.apple.com:80/",
+            },
+            {
+                name: "Microsoft",
+                year: 1998,
+                domain: "https://web.archive.org/web/19981205060735/http://microsoft.com:80/",
+            },
+            {
+                name: "Sony Classical",
+                year: 1998,
+                domain: "https://web.archive.org/web/19980525062302/http://www.sonyclassical.com:80/",
+            },
+            {
+                name: "Fifa World Cup",
+                year: 1998,
+                domain: "https://web.archive.org/web/19990117021251/http://france98.com/",
+            },
+            {
+                name: "Cartoon Network",
+                year: 1999,
+                domain: "https://web.archive.org/web/19991012123959/http://cartoonnetwork.com/",
+            },
+            {
+                name: "KFC",
+                year: 1999,
+                domain: "https://web.archive.org/web/19991013103743/http://kfc.com:80/",
+            },
+            {
+                name: "ABC News",
+                year: 1999,
+                domain: "https://web.archive.org/web/19990428185649/http://abcnews.go.com:80/",
+            },
+            {
+                name: "Britannica",
+                year: 1999,
+                domain: "https://web.archive.org/web/19991117143507/http://britannica.com/",
+            },
+            {
+                name: "Linux",
+                year: 1999,
+                domain: "https://web.archive.org/web/19991109195228/http://www.linux.org/",
+            },
+        ];
+    } catch (err) {}
+};
---
+++ unit-3/backend/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+require("dotenv").config();
+
+// initialise app
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("Server listening on PORT :", PORT);
+});
+
+app.use("/api/v1", domainRoutes);
